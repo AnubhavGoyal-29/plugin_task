75f71f1c23af409aa7584ef0ce104621562fc3b2
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2022-01-25 21:49:45 +0530

    final plugin

--- custom_plugin.php
+++ custom_plugin.php
@@ -21,7 +21,16 @@ Status: true
  */
 include_once( 'wp-admin/includes/plugin.php' );
 function list_all_plugins() {
-
+	if (isset($_GET['deactivate'])) {
+		echo $_GET['key'];
+		deactivate_plugin_via_php($_GET['key']);
+		echo "Successfully deactivated  ". $_GET['key'];
+	}
+	if (isset($_GET['activate'])) {
+		echo $_GET['key'];
+		activate_plugin_via_php($_GET['key']);
+		echo "Successfully activated  ". $_GET['key'];
+	}
 echo '<table margin="10px" border="1px" cellpadding="4" cellspacing="5" padding="5">';
 	$all_plugins = get_plugins();
 	echo '<tr>';
@@ -30,7 +39,11 @@ echo '<table margin="10px" border="1px" cellpadding="4" cellspacing="5" padding=
 	echo ' <th>Version</th>';
 	echo ' <th>Slug</th>';
 	echo ' <th>Status</th>';
-	echo '</tr>';	 
+	echo '</tr>';
+	if(isset($_POST['deactivate'])){
+		echo "deactivate";
+		myFunction(); //here goes the function call
+	 }	 
 	$num = 1;
 	foreach ( $all_plugins as $key => $value ) {
 		echo '<tr>';
@@ -47,12 +60,25 @@ echo '<table margin="10px" border="1px" cellpadding="4" cellspacing="5" padding=
 		echo '<td>'. $plugins[$key]['name'] . '</td>';
 		echo '<td>'. $plugins[$key]['version'] . '</td>';
 		echo '<td>'. $plugins[$key]['slug'] . '</td>';
+		echo '<td>'. $plugins[$key]['status'] . '</td>';
+		
+		if ( $key != "custom_plugin/custom_plugin.php"){
+			if ($plugins[$key]['status'] == "ACTIVE")
+				echo '<td><a href="/wp-admin/admin.php?page=installed-plugin-list&deactivate=true&key='.$key.'">deactivate</a></td>';
+			else echo '<td><a href="/wp-admin/admin.php?page=installed-plugin-list&activate=true&key='.$key.'">activate</a></td>';
+		}
 		$num = $num + 1 ;
 		echo '</tr>';
 
 	}
 	echo '</table>';
 
+}
+function deactivate_plugin_via_php($location) {
+	deactivate_plugins($location);   
+}
+function activate_plugin_via_php($location) {
+	activate_plugins($location);   
 }
 function custom_menu(){
 	add_menu_page('Installed Plugins','Plugin List','manage_options','installed-plugin-list','list_all_plugins','',1);
@@ -61,4 +87,3 @@ function custom_menu(){
 add_action('admin_menu','custom_menu');
 
 ?>
-
